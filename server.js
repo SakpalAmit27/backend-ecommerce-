a7a599911231f51409402645c798e667a898dc9d
Sakpal Amit <amitsakpal744@gmail.com>
2024-09-04 19:27:56 +0530

    basic boiller plate with nodemon setup

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'; 
+
+const app = express();
+
+
+const port = 3000  || process.env.PORT
+
+// get boilerplate // 
+
+app.get('/',(req,res) => {
+    res.send('blacknd admin')
+})
+// listen the port // 
+
+app.listen(port,(req,res) => {
+    console.log(`port is on ${port}`)
+})
