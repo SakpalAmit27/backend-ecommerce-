a8ac3f83044d59d17bd39be3c3c8a1f95046b9a8
Sakpal Amit <amitsakpal744@gmail.com>
2024-09-05 14:34:33 +0530

    code for registering user

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,4 +6,46 @@ import express from 'express'
 
 import User from '../model/userModel.js'
 
+// importing bcrypt // 
+import bcrypt from 'bcrypt'
+
+// crypto // 
+
+import crypto from 'crypto'
+
+//jwt // 
+
+import jwt from 'jsonwebtoken'
+
+
+// an async function to register the user // 
+
+const RegisterUser = async (req,res) => {
+
+    // required essentials // 
+
+    const {name,email,password} = req.body;
+
+    try{
+        //finding if user exists or not // 
+
+        let user = User.findOne({email});
+
+        // if he does // 
+
+        if(user){
+            return res.status(400).json({message:"user already exists , please login"})
+
+
+        }
+
+        // if he doesnt hash pass // 
+
+        const hashedPassword = await bcrypt.hash(password,10);
+
+
+    }
+}
+
+
 const router = express.Router();
